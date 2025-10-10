44233d25f35a98083050fd8b28b63bdefa21f390
ascannel <e.green.3cfg@gmail.com>
2025-10-10 13:19:13 +0300

    basic echo ascii fix

--- bot/db_client.py
+++ bot/db_client.py
@@ -23,5 +23,5 @@ def persistUpdates(updates: list) -> None:
     with connection:
         data = []
         for update in updates:
-            data.append((json.dumps(update),))
-        connection.executemany("INSERT INTO telegram_updates (payload) VALUES(?)", data,)
+            data.append((json.dumps(update, ensure_ascii=False),))
+        connection.executemany("INSERT INTO telegram_updates (payload) VALUES (?)", data,)
